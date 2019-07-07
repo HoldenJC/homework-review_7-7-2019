5f55775dc2bdb04626d7372ae1b4e73b68d87ce0
HoldenJC <holdenj_c@yahoo.com>
2019-07-07 16:14:02 -0700

    add asynchrony homework review code

--- src/backend-code.js
+++ src/backend-code.js
@@ -1,6 +1,9 @@
+import $ from 'jquery';
+
 export class Example {
   constructor(){
     this.name = "Holden";
+    this.favColor = "Green";
   }
 
   exampleMethod(){
@@ -8,4 +11,32 @@ export class Example {
   }
 }
 
+export let cat = {
+  meowLevel: 10,
+  setLoudness: function() {
+    const loudnessLevel = setInterval(() => {
+      this.meowLevel++;
+      $("#test").append(`<br>${this.meowLevel}`);
+      if (this.didYouGoDeaf() === true) {
+        clearInterval(loudnessLevel);
+        $("#test").append(`<br>You went deaf from all the meowing!`);
+      }
 
+    }, 1000);
+  },
+  didYouGoDeaf: function() {
+    if (this.meowLevel < 20){
+      return false;
+    } else {
+      return true; 
+    }
+  },
+  pet: function(calmCat) {
+    let that = this;
+    return function(meow) {
+      that.meowLevel -= calmCat;
+      $("#test").append(`<br>The cat enjoyed the petting. ${meow}! Meowing desire goes down ${calmCat}!`);
+      $("#test").append(`<br>${this.meowLevel}`);
+    };
+  }
+};
--- src/main.js
+++ src/main.js
@@ -2,12 +2,78 @@ import $ from 'jquery';
 import './styles.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Example } from './backend-code';
+import { Example, cat } from './backend-code';
 
 let example = new Example();
 
 $(document).ready(function() {
-  $("#test").append(example.name);
+
+  example.introduction = function() {
+    $("#test").append(`<br>My name is ${this.name}`);
+  }
+
+  example.introduction2 = function() {
+    $("#test").append(`<br>My name is ${this.name}`);
+    function doThis() {
+      try {$("#test").append(`<br>My name is ${this.name}`);}
+      catch(error) {$("#test").append(`<br>error: ${error.message}`);}
+    }
+    doThis();
+  }
+
+  example.introduction3 = function() {
+    $("#test").append(`<br>My name is ${this.name}`);
+    let that = this;
+    function doThis() {
+      try {$("#test").append(`<br>My name is ${that.name}`);}
+      catch(error) {$("#test").append(`<br>error: ${error.message}`);}
+    }
+    doThis();
+  }
+
+  example.introduction4 = function() {
+    $("#test").append(`<br>My name is ${this.name}`);
+    let doThis = () => { 
+      try {$("#test").append(`<br>My name is ${this.name}`);}
+      catch(error) {$("#test").append(`<br>error: ${error.message}`);}
+    }
+    doThis();
+  }
+
+  function welcome(name) {
+    return function(color) {
+      $("#test").append(`<br>${name}'s favorite color is: ${color}`);
+    }
+  }
+
+  example.introduction();
   example.exampleMethod();
-  $("#test").append(example.name);
+  example.introduction();
+  example.introduction2();
+  example.introduction3();
+  example.introduction4();
+  let holdenDude = welcome(`${example.name} the dude's`);
+  console.log(holdenDude);
+  holdenDude();
+  holdenDude("green, followed by purple then pink");
+
+  let olive = cat;
+  $("#test").append(`<br>${olive.meowLevel}`);
+  olive.setLoudness();
+  cat.petSmall = cat.pet(5);
+  cat.petMedium = cat.pet(7);
+  cat.petLarge = cat.pet(10);
+
+  setTimeout(function(){
+    olive.petSmall('Pur!');
+    setTimeout(function(){
+      olive.petMedium('Purr!');
+      setTimeout(function(){
+        olive.petLarge('Purrr!');
+      }, 5000);
+    }, 5000);
+  }, 5000);
+  
+  
+  
 });
